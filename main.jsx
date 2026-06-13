f49e152973457c6663a187438005c200d4e4b684
garuharu0321 <garuharu0321@gmail.com>
2026-06-13 19:46:44 +0900

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PetknockForm from './PetknockForm'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PetknockForm />
+  </React.StrictMode>,
+)
